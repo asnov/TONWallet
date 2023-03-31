26d2b327b9d8608060070746d434f41096fa40f8
Alex <alex@superiordigital.design>
2023-03-31 12:16:27 -0300

    added image;

--- app/src/main/java/com/example/tonwallet/MainActivity.kt
+++ app/src/main/java/com/example/tonwallet/MainActivity.kt
@@ -5,17 +5,17 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
 import com.example.tonwallet.ui.theme.TONWalletTheme
@@ -39,14 +39,18 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, from: String) {
-    Column {
+fun Greeting(name: String, from: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.tonimage)    // drawable.tonimage ???
+    Column(modifier) {
         Row(
             verticalAlignment = Alignment.Bottom,
             horizontalArrangement = Arrangement.SpaceBetween,
         ) {
             Text("Hello", fontSize = 18.sp)
             Text("$name!", fontSize = 24.sp)
+        }
+        Row {
+            Image(image, "tonImage", Modifier.width(100.dp).height(100.dp))
         }
         Row(
             verticalAlignment = Alignment.Bottom,
@@ -62,6 +66,8 @@ fun Greeting(name: String, from: String) {
     name = "Day Mode",
     showSystemUi = true,
     uiMode = UI_MODE_NIGHT_NO,
+    widthDp = 360,
+    heightDp = 640,
 )
 @Composable
 fun DefaultPreview() {
