95ce66210167a6434bb0eb5c3906c5e867d7cf52
CIBOBTC <7309407@gmail.com>
2023-05-06 16:56:07 -0300

    Wallet Main Loading page done

--- app/src/main/java/com/example/tonwallet/pages/WalletMainTransactionsPage.kt
+++ app/src/main/java/com/example/tonwallet/pages/WalletMainTransactionsPage.kt
@@ -43,7 +43,7 @@ fun WalletMainTransactionsPage(
     Column(
         modifier
             .background(Color.Black),
-        Arrangement.Center,
+        Arrangement.Bottom,
         Alignment.CenterHorizontally,
     ) {
         Row(
