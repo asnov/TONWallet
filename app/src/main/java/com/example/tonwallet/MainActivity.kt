062223672778970038689512a14905d164fb6788
Alex <alex@superiordigital.design>
2023-05-10 13:06:43 -0300

    made Import start page;

--- app/src/main/java/com/example/tonwallet/MainActivity.kt
+++ app/src/main/java/com/example/tonwallet/MainActivity.kt
@@ -1,24 +1,31 @@
 package com.example.tonwallet
 
+//import android.os.Build
 import android.os.Bundle
 import android.util.Log
+//import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+//import androidx.annotation.RequiresApi
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
 
 
 private const val TAG = "MainActivity"
-internal val StatusBarHeight = 28.dp
+internal val StatusBarHeight = 0.dp
 internal val NavigationBarHeight = 20.dp     // TODO: check if it is 48
 // TODO: get size of unused bottom part of the screen
 
 val Roboto = FontFamily(Font(R.font.roboto))
 
 class MainActivity : ComponentActivity() {
+//    @RequiresApi(Build.VERSION_CODES.R)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+//        window.setDecorFitsSystemWindows(false)
+//        window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE)
         setContent {
             Log.v(TAG, "started")
             Navigation()
--- app/src/main/java/com/example/tonwallet/pages/ImportStartPage.kt
+++ app/src/main/java/com/example/tonwallet/pages/ImportStartPage.kt
@@ -2,25 +2,35 @@ package com.example.tonwallet.pages
 
 import android.content.res.Configuration
 import android.util.Log
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Card
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.material.TextFieldDefaults.textFieldColors
 import androidx.compose.runtime.*
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.layout.positionInParent
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
@@ -31,16 +41,22 @@ import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Popup
-import com.example.tonwallet.NavigationBarHeight
+import androidx.compose.ui.zIndex
 import com.example.tonwallet.PanelHeader
 import com.example.tonwallet.R
 import com.example.tonwallet.Roboto
+import com.example.tonwallet.StatusBarHeight
 import com.example.tonwallet.ui.theme.TONWalletTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 
 private const val TAG = "ImportStartPage"
 
-@OptIn(ExperimentalMaterialApi::class)
+@OptIn(
+    ExperimentalMaterialApi::class, ExperimentalFoundationApi::class,
+    ExperimentalComposeUiApi::class
+)
 @Composable
 fun ImportStartPage(
     goBack: () -> Unit,
@@ -50,35 +66,88 @@ fun ImportStartPage(
 ) {
     Log.v(TAG, "started")
 
-    val numberOfTheWordsToEnter = arrayOf(5, 15, 18)
-//    var words = numberOfTheWordsToEnter.map { wordList[it - 1] }.toTypedArray()
-    val (words, setWords) = remember { mutableStateOf(arrayOf("", "", "")) }
-//    var words = arrayOf("", "", "").toList().toMutableStateList()
-//    var words2 by remember { mutableListOf("", "", "") }
-//    var words3 by remember { mutableStateListOf("", "", "") }
-//    var mut: State<List<String>>
-//    var words4 by remember { mut }
-    var isPopupVisible by remember { mutableStateOf(false) }
+    val numberOfTheWordsToEnter = 24
+    val words by remember { mutableStateOf(Array(numberOfTheWordsToEnter) { "" }) }
+    val focusRequesters = remember { Array(numberOfTheWordsToEnter) { FocusRequester() } }
 
-    Column(modifier, horizontalAlignment = Alignment.CenterHorizontally) {
-        PanelHeader(goBack)
-        Column(
-            Modifier.verticalScroll(rememberScrollState()),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            Image(painterResource(R.drawable.sticker_recovery_page), null, Modifier.size(100.dp))
-            Text(
-                ("24 Secret Words"),
-                Modifier.padding(vertical = 12.dp),
-                Color(0xFF222222),
-                fontFamily = Roboto,
-                fontWeight = FontWeight.W500,
-                fontSize = 24.sp,
-                lineHeight = 28.sp,
-                textAlign = TextAlign.Center,
+    Log.v(TAG, focusRequesters.toString())
+    Log.v(TAG, focusRequesters[0].toString())
+    Log.v(TAG, focusRequesters[23].toString())
+    Log.v(TAG, (focusRequesters[0] == FocusRequester.Default).toString())
+    Log.v(TAG, (focusRequesters[0] == FocusRequester.Cancel).toString())
+
+    var isPopupVisible by remember { mutableStateOf(false) }
+    val coroutineScope = rememberCoroutineScope()
+
+    val state: LazyListState = rememberLazyListState()
+
+    val firstVisibleItemIndex = remember { derivedStateOf { state.firstVisibleItemIndex } }
+    print("state.firstVisibleItemIndex:${firstVisibleItemIndex.value}, ")
+    var firstVisibleItemScrollOffset = 0
+    LaunchedEffect(state) {
+        snapshotFlow { state.firstVisibleItemScrollOffset }
+            .collect { firstVisibleItemScrollOffset = it }
+    }
+    println("state.firstVisibleItemScrollOffset:$firstVisibleItemScrollOffset")
+
+    PanelHeader(
+        goBack,
+        Modifier
+            .background(Color(0x00FFFFFF))
+            .zIndex(2f)
+    )
+
+    LazyColumn(
+        modifier
+//            .imePadding()
+            .padding(top = StatusBarHeight),
+        state,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+
+
+        item {
+            Spacer(
+                Modifier
+                    .height(56.dp)
+                    .fillParentMaxWidth()
+                    .background(Color(0xFFFFFFFF))
             )
+            Image(
+                painterResource(R.drawable.sticker_recovery_page),
+                null,
+                Modifier.size(100.dp)
+            )
+        } // item
+
+        var elevation by mutableStateOf(0.dp)
+        stickyHeader {
+            Surface(
+                Modifier.onGloballyPositioned {
+                    elevation = if (it.positionInParent().y < 1f) 4.dp else 0.dp
+                },
+                elevation = elevation,
+            ) {
+                Text(
+                    stringResource(R.string._24_secret_words),
+                    Modifier
+                        .padding(vertical = 12.dp)
+                        .background(Color(0xFFFFFFFF))
+                        .fillMaxWidth(),
+                    Color(0xFF222222),
+                    fontFamily = Roboto,
+                    fontWeight = FontWeight.W500,
+                    fontSize = 24.sp,
+                    lineHeight = 28.sp,
+                    textAlign = TextAlign.Center,
+                )
+            }
+        } // stickyHeader
+
+        item {
+
             Text(
-                "You can restore access to your wallet by entering 24 words you wrote when down you creating the wallet.",
+                stringResource(R.string.you_can_restore_access),
                 Modifier.fillMaxWidth(280 / 360f),
                 Color(0xFF000000),
                 fontFamily = Roboto,
@@ -88,9 +157,10 @@ fun ImportStartPage(
                 textAlign = TextAlign.Center,
             )
             Text(
-                text = "I don't have those",
+                text = stringResource(R.string.i_don_t_have_those),
                 modifier = Modifier
                     .fillMaxWidth(280 / 360f)
+                    .clickable(onClick = goNoPhrase)
                     .padding(top = 20.dp),
                 color = Color(0xFF339CEC),
                 fontFamily = Roboto,
@@ -100,92 +170,120 @@ fun ImportStartPage(
                 letterSpacing = 0.1.sp,
                 textAlign = TextAlign.Center,
             )
-            Column(
+            Spacer(modifier = Modifier.height(20.dp))
+
+        } // item
+
+        items(numberOfTheWordsToEnter) { index ->
+            var currentWord by remember { mutableStateOf(words[index]) }
+            val interaction = MutableInteractionSource()
+            Row(
                 Modifier
-                    .padding(vertical = 28.dp)
+                    .focusRequester(focusRequesters[index])
                     .fillMaxWidth(200 / 360f),
-                verticalArrangement = Arrangement.spacedBy(8.dp),
+                verticalAlignment = Alignment.CenterVertically,
             ) {
+                TextField(
+                    currentWord,
+                    {
+                        currentWord = it
+                        words[index] = it
+                        // TODO: showContextMenuIfNecessary
 
-
-                numberOfTheWordsToEnter.forEachIndexed { index, word ->
-                    var currentWord by remember { mutableStateOf(words[index]) }
-                    currentWord = words[index]
-                    val interaction = MutableInteractionSource()
-
-                    Row(
-                       verticalAlignment = Alignment.CenterVertically,
-                        ) {
-                        TextField(
-                            "currentWord",
-                            {},
-                            Modifier
-                                .fillMaxWidth()
-                                .height(68.dp),
-                            textStyle = TextStyle(
-                                color = Color(0xFF000000),
-                                fontFamily = Roboto,
-                                fontWeight = FontWeight.W400,
-                                fontSize = 15.sp,
-                                lineHeight = 20.sp,
-                                textAlign = TextAlign.Left ,
-                                ),
-                            label = {},
-                            placeholder = {},
-                            leadingIcon = {
-                                Text(
-                                    "15:",
-                                    Modifier.width(44.dp),
-                                    textAlign = TextAlign.Right,
-                                )
-                            },
-                            trailingIcon = {},
-                            singleLine = true,
-                            interactionSource = interaction,
-                            colors = textFieldColors(
-                                backgroundColor = Color(0xFFFFFFFF),
-                                cursorColor = Color(0xFF339CEC),
-                                focusedIndicatorColor = Color(0xFF339CEC),
-                                unfocusedIndicatorColor = Color(0xFFDBDBDB),
-                                leadingIconColor = Color(0xFF757575),
-                                focusedLabelColor = Color.Red,
-                                placeholderColor = Color.LightGray,
-                            ),
+                    },
+                    Modifier
+                        .fillMaxWidth()
+                        .height(68.dp),
+                    textStyle = TextStyle(
+                        color = Color(0xFF000000),
+                        fontFamily = Roboto,
+                        fontWeight = FontWeight.W400,
+                        fontSize = 15.sp,
+                        lineHeight = 20.sp,
+                        textAlign = TextAlign.Left,
+                    ),
+                    label = {},
+                    placeholder = {},
+                    leadingIcon = {
+                        Text(
+                            "${index + 1}:",
+                            Modifier.width(44.dp),
+                            textAlign = TextAlign.Right,
                         )
+                    },
+                    trailingIcon = {},
+                    singleLine = true,
+                    interactionSource = interaction,
+                    colors = textFieldColors(
+                        backgroundColor = Color(0xFFFFFFFF),
+                        cursorColor = Color(0xFF339CEC),
+                        focusedIndicatorColor = Color(0xFF339CEC),
+                        unfocusedIndicatorColor = Color(0xFFDBDBDB),
+                        leadingIconColor = Color(0xFF757575),
+                        focusedLabelColor = Color.Red,
+                        placeholderColor = Color.LightGray,
+                    ),
+                )
+            }   // Row
+        } // items
 
 
-                    }   // Row
-                }
-                if (isPopupVisible) {
-                    Popup(offset = IntOffset(0, -80)) {
-                        Card(onClick = { /*TODO*/ }) {
-                            Row(
-                                Modifier
-                                    .border(width = 1.dp, color = Color(0x19000000))
-                            ) {
-                                Text("mafioso", Modifier.padding(16.dp, 12.dp))
-                                Text("mafia", Modifier.padding(16.dp, 12.dp))
-                                Text("maffin", Modifier.padding(16.dp, 12.dp))
-                            }
+        item {
+
+            if (isPopupVisible) {
+                Popup(offset = IntOffset(0, -80)) {
+                    Card(onClick = { /*TODO*/ }) {
+                        Row(
+                            Modifier
+                                .border(width = 1.dp, color = Color(0x19000000))
+                        ) {
+                            Text("mafioso", Modifier.padding(16.dp, 12.dp))
+                            Text("mafia", Modifier.padding(16.dp, 12.dp))
+                            Text("maffin", Modifier.padding(16.dp, 12.dp))
                         }
                     }
                 }
-
             }
-            // TODO: make button visible while using keyboard
+
+            Spacer(modifier = Modifier.height(28.dp))
+
             Button(
                 {
-                    if (isPopupVisible) goForth()
-                    isPopupVisible = true
-                    // FIXME: it doesn't update words
-                    // words = ...
-//                    setWords(numberOfTheWordsToEnter.map { wordList[it - 1] }.toTypedArray())
+                    Log.v(TAG, focusRequesters.toString())
+                    Log.v(TAG, focusRequesters[0].toString())
+                    Log.v(TAG, focusRequesters[23].toString())
+                    Log.v(TAG, (focusRequesters[0] == FocusRequester.Default).toString())
+                    Log.v(TAG, (focusRequesters[0] == FocusRequester.Cancel).toString())
+
+                    print("state.firstVisibleItemIndex:${firstVisibleItemIndex.value}, ")
+                    println("state.firstVisibleItemScrollOffset:$firstVisibleItemScrollOffset")
+
+                    val indexOfFirstBlankField = words.indexOfFirst { it.isBlank() }
+                    if (indexOfFirstBlankField == -1) {
+                        goForth()
+                    } else {
+                        // go to the first empty word and make it focused
+                        coroutineScope.launch {
+//                            Log.v(TAG, "scrolling to ${indexOfFirstBlankField + 1}")
+//                            state.scrollToItem(indexOfFirstBlankField + 1)
+                            Log.v(TAG, "animate scrolling to ${indexOfFirstBlankField + 1}")
+                            state.animateScrollToItem(indexOfFirstBlankField + 1)
+                            Log.v(TAG, "stoped scrolling")
+                            delay(100)
+                            // TODO: make sure node.isAttached
+                            // androidx/compose/ui/node/DelegatableNode.kt:206
+                            // androidx/compose/ui/node/DelegatableNode.kt:39
+                            Log.v(TAG, "start focusing")
+                            focusRequesters[indexOfFirstBlankField].requestFocus()
+                            Log.v(TAG, "stopped focusing")
+
+                        }
+                    }
+                    isPopupVisible = false
                 },
                 Modifier
                     .fillMaxWidth(200 / 360f)
-                    .padding(
-                        bottom = 100.dp + NavigationBarHeight,
-                    ),
+                    .padding(bottom = 56.dp),
                 colors = ButtonDefaults.buttonColors(
                     backgroundColor = Color(0xFF339CEC),
                     contentColor = Color(0xFFFFFFFF),
@@ -202,8 +300,10 @@ fun ImportStartPage(
                     lineHeight = 20.sp,
                 )
             }
-        }
-    }
+        } // item
+
+
+    } // LazyColumn()
 }
 
 @Preview(
@@ -216,6 +316,6 @@ fun ImportStartPage(
 @Composable
 private fun DefaultPreview() {
     TONWalletTheme {
-        ImportStartPage({}, {}, {})
+        ImportStartPage({}, {}, {}, Modifier.background(Color(0xFFFFFFFF)))
     }
 }
