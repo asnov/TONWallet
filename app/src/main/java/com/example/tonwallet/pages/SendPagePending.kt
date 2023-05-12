4541efb8a8543fd8d32ea3c42b8a1f97edb0dbe4
Alex Novikov <7309407@gmail.com>
2023-05-12 14:59:05 -0300

    Send Page Pending 6.3 done

--- app/src/main/java/com/example/tonwallet/pages/SendPagePending.kt
+++ app/src/main/java/com/example/tonwallet/pages/SendPagePending.kt
@@ -11,31 +11,23 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.tonwallet.NavigationBarHeight
-import com.example.tonwallet.PanelHeader
 import com.example.tonwallet.R
 import com.example.tonwallet.Roboto
 import com.example.tonwallet.StatusBarHeight
@@ -61,7 +53,7 @@ fun SendPagePending(
                         bottomEnd = 0.dp
                     )
                 )
-                //.padding(top=8.dp)
+                .padding(top=8.dp)
                 .fillMaxWidth()
                 .fillMaxHeight(),
         )
